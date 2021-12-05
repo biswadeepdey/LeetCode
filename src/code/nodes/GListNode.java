4943804675a21b0e0da34aef799f04925ab74037
Biswadeep Dey <biswa8998@gmail.com>
2021-12-05 23:55:06 +0530

    Started again on AllOne :)

--- src/code/nodes/GListNode.java
+++ src/code/nodes/GListNode.java
@@ -3,6 +3,7 @@ package code.nodes;
 public class GListNode<T> {
 	public T val;
 	public GListNode<T> next;
+	public GListNode<T> previous;
 	
 	public GListNode() {}
 	public GListNode(T val) {this.val = val;}
